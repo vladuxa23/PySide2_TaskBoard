4416b9eaa3b33267ed3b8294399d401e09d4b7cf
vladuxa23 <vlad_1995.02@mail.ru>
2021-11-14 12:58:04 +0300

    add new task at category

--- TaskBoard.py
+++ TaskBoard.py
@@ -1,8 +1,8 @@
 import sys
-from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2 import QtCore, QtWidgets
 
 from ui.TaskBoard_design import Ui_TaskBoard
-from ui.TaskWidget import TaskWidget
+from TaskWidget import TaskWidget
 from NewTask import NewTask
 
 
@@ -20,21 +20,25 @@ class TaskBoard(QtWidgets.QMainWindow):
         self.ui.pushButton.clicked.connect(self.show_new_task_widget)
         self.ui.actionExit.triggered.connect(lambda: self.close())
 
-        self.widgetDo = QtWidgets.QWidget()
+        self.widgetDo = QtWidgets.QGroupBox("Сделать")
         self.widgetDo.setObjectName("WidgetDo")
         self.widgetDo.setLayout(QtWidgets.QVBoxLayout())
 
-        self.widgetDoing = QtWidgets.QWidget()
+        self.widgetDoing = QtWidgets.QGroupBox("В процессе")
         self.widgetDoing.setObjectName("WidgetDoing")
         self.widgetDoing.setLayout(QtWidgets.QVBoxLayout())
 
-        self.widgetDone = QtWidgets.QWidget()
+        self.widgetDone = QtWidgets.QGroupBox("Выполнено")
         self.widgetDone.setObjectName("WidgetDone")
         self.widgetDone.setLayout(QtWidgets.QVBoxLayout())
 
         self.ui.scrollAreaDo.setWidget(self.widgetDo)
         self.ui.scrollAreaDoing.setWidget(self.widgetDoing)
         self.ui.scrollAreaDone.setWidget(self.widgetDone)
+
+        self.stretchy_spacer_thing = QtWidgets.QSpacerItem(10, 10,
+                                                           QtWidgets.QSizePolicy.Minimum,
+                                                           QtWidgets.QSizePolicy.Expanding)
 
     def show_new_task_widget(self):
         self.new_task_widget = NewTask()
@@ -45,7 +49,25 @@ class TaskBoard(QtWidgets.QMainWindow):
         self.new_task_widget.show()
 
     def add_new_task(self, data):
-        print(data)
+
+        widget = self.getWidgetByObjectName(data["category"])
+
+        try:
+            widget.layout().removeItem(self.stretchy_spacer_thing)
+        except Exception as err:
+            print(err)
+
+        widget.layout().addWidget(TaskWidget(**data))
+        widget.layout().addItem(self.stretchy_spacer_thing)
+
+    @staticmethod
+    def getWidgetByObjectName(widget_name):
+        widgets = QtWidgets.QApplication.instance().topLevelWidgets()
+        widgets = widgets + QtWidgets.QApplication.instance().allWidgets()
+        for x in widgets:
+            print(x.objectName())
+            if str(x.objectName()) == widget_name:
+                return x
 
 
 if __name__ == '__main__':
